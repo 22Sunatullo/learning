33bffe77ca479f14b6eee84cd7625b3fed8df5eb
adilovcode <adilovcode@gmail.com>
2022-12-24 14:58:32 +0500

    Some modifications were added

--- js validation form/js/app.js
+++ js validation form/js/app.js
@@ -1,92 +1,59 @@
+
+
+
 // get tag from html
 const btn = document.querySelector('.btn');
 
+const validations = [
+    {
+        element: document.querySelector('.em'),
+        validation: didEmailPassedValidation
+    },
+    {
+        element: document.querySelector('.pas'),
+        validation: didPasswordPassesValidation
+    },
+    {
+        element: document.querySelector('.tex'),
+        validation: didFullNamePassedValidation
+    },
+    {
+        element: document.querySelector('.num'),
+        validation: didPhoneNumberPassedValidation
+    }
+]
 
-let em  = document.querySelector('.em');
-let pas = document.querySelector('.pas');
-let tex = document.querySelector('.tex');
-let num = document.querySelector('.num');
- 
-// add event
+btn.addEventListener('click', function (e){
 
-btn.addEventListener('click', function (){
-    emailVAlidation();
-    passwordVAlidation ();
-    nameValidation();
-    numberVAlidation();
+    validations.forEach(validationItem => {
+        if (validationItem.validation()) {
+            validationItem.element.innerHTML = 'you are right';
+            validationItem.element.style.color = 'green'
+        } else {
+            validationItem.element.innerHTML = 'noo';
+            validationItem.element.style.color = 'red'
 
+            e.preventDefault();
+        }
+    })
 })
 
-
-
-// validation for email
-function emailVAlidation(){
+function didEmailPassedValidation() {
     let email = document.querySelector('.email').value;
-    if(email.length > 3 && email.length < 30 && email.includes('@') && email.includes('mail.com')){
-        em.innerHTML = 'you are right';
-        em.style.color = 'green'
-    }
-    
-     else {
-        em.innerHTML = 'noo';
-        em.style.color = 'red'
-        return;
-     }
+    return email.length > 3 && email.length < 30 && email.includes('@') && email.includes('mail.com');
 }
 
-
-// validation for password
-
-function passwordVAlidation (){
+function didPasswordPassesValidation(){
     const password = document.querySelector('.password').value;
-    if(password.length > 8 && password.length < 30){
-        pas.innerHTML = 'you are right';
-        pas.style.color = 'green'
-    }
-
-    // if(password.includes(' ')){
-    //     pas.innerHTML = 'noo';
-    //     pas.style.color = 'red';
-    //     return;
-    // }
-     else {
-        pas.innerHTML = 'noo';
-        pas.style.color = 'red'
-        return;
-     } 
+    return password.length > 8 && password.length < 30;
 }
 
-
-// validation for name
-
-function nameValidation(){
+function didFullNamePassedValidation(){
     const text = document.querySelector('.text').value;
- 
-    if(text.length > 3 && text.length < 30 && text.includes(' ')){
-        tex.innerHTML = 'you are right';
-        tex.style.color = 'green'
-    }
-    
-     else {
-        tex.innerHTML = 'noo';
-        tex.style.color = 'red'
-        return;
-     }
-
+    return text.length > 3 && text.length < 30 && text.includes(' ');
 }
 
-
-// validation for number
-function numberVAlidation(){
+function didPhoneNumberPassedValidation(){
     const number = document.querySelector('.number').value;
-    if(number.length == 9){
-        num.innerHTML = 'you are right';
-        num.style.color = 'green'
-    }
-
-    else{
-        num.innerHTML = 'noo';
-        num.style.color = 'red'
-        return; 
-    }
+    return number.length == 9;
 }
